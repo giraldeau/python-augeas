163821a3b420ab150f880c83ea389bf5e800d890
Francis Giraldeau <francis.giraldeau@gmail.com>
2010-10-06 21:55:31 -0400

    Add library overloading with LD_LIBRARY_PATH
        When testing with a special build of augeas, we need to overload the standard
        library on the system with LD_LIBRARY_PATH, but ctypes.find_lib doesn't return
        them. The fix works for linux 32 bits.
    
        The problem is documented here: http://bugs.python.org/issue9998

--- augeas.py
+++ augeas.py
@@ -41,12 +41,30 @@ Nils Philippsen <nils@redhat.com>
 import ctypes
 import ctypes.util
 from sys import version_info as _pyver
+import string
+import glob
+import os
+import sys 
+
+def _findLib(name):
+    ld_path = os.getenv("LD_LIBRARY_PATH")
+    for p in string.split(ld_path, ":"):
+        if (p[:-1] != "/"):
+            p += "/"
+        path = "%slib%s*.so" % (p,name)
+        files = glob.glob(path)
+        files.sort()
+        if (len(files) > 0):
+            return os.path.basename(files[0])
+    return None
 
 def _dlopen(*args):
     """Search for one of the libraries given as arguments and load it.
     Returns the library.
     """
     libs = [l for l in [ ctypes.util.find_library(a) for a in args ] if l]
+    if (len(libs) == 0):
+        libs = libs = [l for l in [ _findLib(a) for a in args ] if l]
     lib  = reduce(lambda x, y: x or ctypes.cdll.LoadLibrary(y), libs, None)
     if not lib:
         raise ImportError("Unable to import lib%s!" % args[0])
